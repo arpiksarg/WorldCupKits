9a75d9edb4ff6927f877636fdc9b9042d2818319
Arpik Sargsyan <arpi@xamlab.com>
2019-01-04 14:55:42 +0400

    Added Model, View, Controller folders to the project

---
+++ WorldCupKits/WorldCupKits/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorldCupKits
+//
+//  Created by Arpik Sargsyan on 1/4/19.
+//  Copyright © 2019 Arpik Sargsyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
