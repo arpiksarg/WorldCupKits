f87f1bf041cb7a1be31028b59bc485b71a6f17bc
Arpik Sargsyan <arpi@xamlab.com>
2019-01-10 13:51:53 +0400

    Added shirtStore array to store data

--- WorldCupKits/WorldCupKits/Controller/StoreCollectionViewController.swift
+++ WorldCupKits/WorldCupKits/Controller/StoreCollectionViewController.swift
@@ -10,7 +10,15 @@ import UIKit
 
 class StoreCollectionViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
-    var shirtStore: [StoreMO] = []
+    var shirtStore: [Store] = [Store(countryName: "Spain", cityName: "Barcelona", brand: "Nike", price: "200$", shirtImage: "image_1"),
+                               Store(countryName: "Spain", cityName: "Barcelona", brand: "Nike", price: "200$", shirtImage: "image_2"),
+                               Store(countryName: "Spain", cityName: "Barcelona", brand: "Nike", price: "200$", shirtImage: "image_1"),
+                               Store(countryName: "Spain", cityName: "Barcelona", brand: "Nike", price: "200$", shirtImage: "image_1"),
+                               Store(countryName: "Spain", cityName: "Barcelona", brand: "Nike", price: "200$", shirtImage: "image_1"),
+                               Store(countryName: "Spain", cityName: "Barcelona", brand: "Nike", price: "200$", shirtImage: "image_1"),
+                               Store(countryName: "Spain", cityName: "Barcelona", brand: "Nike", price: "200$", shirtImage: "image_1"),
+                               Store(countryName: "Spain", cityName: "Barcelona", brand: "Nike", price: "200$", shirtImage: "image_1")
+                            ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,13 +26,17 @@ class StoreCollectionViewController: UIViewController, UICollectionViewDelegate,
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
+        return shirtStore.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionCell", for: indexPath) as! StoreCollectionViewCell
         cell.shirtImageView.image = UIImage(named: "image_1")
+        cell.brandLabel.text = shirtStore[indexPath.row].brand
+        cell.cityNameLabel.text = shirtStore[indexPath.row].cityName
+        cell.countryNameLabel.text = shirtStore[indexPath.row].countryName
+        cell.priceLabel.text = shirtStore[indexPath.row].price
         return cell
     }
 }
